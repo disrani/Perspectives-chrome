602c309cf5dec2be5dfce395aa3e7bc777e8abaa
Admin <Admin@cmu-427862.wv.cc.cmu.edu>
2010-10-27 21:32:22 -0400

    Fixed applet for chrome.

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 var app = document.createElement("applet");
-app.setAttribute("name", "SSLCert");
-app.setAttribute("height", "0");
-app.setAttribute("width", "0");
+app.setAttribute("height", "1");
+app.setAttribute("width", "1");
 app.setAttribute("code", "SSLCert");
 app.setAttribute("archive", "https://www.andrew.cmu.edu/user/ddi/SSLjar.jar");
 app.setAttribute("codebase", ".");
 app.setAttribute("id", "SSLCert");
+app.setAttribute("mayscript", "true");
 document.body.appendChild(app);
 
 chrome.extension.onRequest.addListener(
@@ -15,15 +15,16 @@ chrome.extension.onRequest.addListener(
 			setTimeout(function() {
 				var fp = null;
 				var s = document.getElementById("SSLCert");
+				console.log(s);
 				try {
 			        fp = s.getCertFingerprint(request.host, request.port);
 					console.log(fp);
 			    } catch(e) {
-			        alert("content error:"+e);
+			        alert("content error:"+e.name+":"+e.message);
 			    }
 				document.body.removeChild(s);
 				sendResponse(fp);
-				}, 2000);
+			}, 2000);
 		} else if (request.action == "remove_applet") {
 			var s = document.getElementById("SSLCert");
 			if (s != null) {
